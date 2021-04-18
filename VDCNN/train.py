d4357143baaa6d4d5080245b229164f0e1add072
sungwoon.kim <sungwoon.kim@gausslabs.ai>
2021-04-18 14:59:36 +0900

    test

--- VDCNN/train.py
+++ VDCNN/train.py
@@ -18,6 +18,7 @@ def get_params(cfgpath):
         params = json.loads(io.read())
     return params
 
+
 def load_data(params):     
     tokenizer = JamoTokenizer()
     padder = PadSequence(length=params.get('pad_length'))
